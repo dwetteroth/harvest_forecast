317cf71c62a7c65a1fdb9b47614d729982f15d2c
Daniel C Wetteroth <danielcw@gmail.com>
2017-12-11 16:24:38 -0700

    0.0.2 version commit

---
+++ harvest_forecast.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name        = 'harvest_forecast_api'
+  s.version     = '0.0.2'
+  s.date        = '2017-12-01'
+  s.summary     = "Forecast API Creation."
+  s.description = "API wrapper for Forecast"
+  s.authors     = ["Daniel Wetteroth"]
+  s.email       = 'danielcw@gmail.com'
+  s.files       = ["lib/main.rb", "README.md", "lib/forecast.rb", "lib/forecast/base.rb"]
+  s.has_rdoc    = true
+  s.extra_rdoc_files  = ["README.rdoc"]
+  s.rdoc_options      = ["--main", "README.rdoc"]
+  s.homepage    =
+    'http://rubygems.org/gems/harvest_forecast_api'
+  s.license       = 'MIT'
+end
---
+++ lib/forecast.rb
@@ -0,0 +1,30 @@
+module Forecast
+  VERSION   = "0.0.2"
+  ApiDomain = "forecastapp.com"
+
+  # Class method to load all ruby files from a given path.
+  def self.load_all_ruby_files_from_path(path)
+    Dir.foreach(path) do |file|
+      require File.join(path, file) if file =~ /\.rb$/
+    end
+  end
+
+end
+
+# Gems
+require "rubygems"
+require "activeresource"
+require "active_resource_throttle"
+
+# Plugins
+PluginPath = File.join(File.dirname(__FILE__), "forecast", "plugins")
+Forecast.load_all_ruby_files_from_path(PluginPath)
+
+# Base
+require File.join(File.dirname(__FILE__), "forecast", "base")
+require File.join(File.dirname(__FILE__), "forecast", "forecast_resource")
+
+# Shortcut for Forecast::Base.new
+def Forecast(options={})
+  Forecast::Base.new(options)
+end
